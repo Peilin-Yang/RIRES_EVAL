32c30b3972bbe9d6157c282f9f9d42b540492616
Peilin Yang <yangpeilyn@gmail.com>
2017-06-02 12:32:54 -0400

    add pertube to eval.py

--- eval.py
+++ eval.py
@@ -34,12 +34,16 @@ def replace_and_make(fn):
     #print_debug('Compile Success!!')
 
 
-def run_query(index_path, query_file_path):
+def run_query(index_path, query_file_path, pertube_type='0', pertube_paras_str=''):
     container_index_path = os.path.join(index_folder, os.path.basename(index_path))
     query_folder_path = os.path.join(query_folder, os.path.basename(query_file_path) )
     for qf in os.listdir(query_folder_path):
         container_query_file_path = os.path.join(query_folder_path, qf)
-        p = Popen(['./runquery/IndriRunQuery', container_query_file_path, '-index='+container_index_path], 
+        p = Popen(['./runquery/IndriRunQuery', 
+                container_query_file_path, 
+                '-index=%s' % container_index_path], 
+                '-pertube=%s' % pertube_type,
+                '-pertube_paras=%s' % pertube_paras_str,
                 stdout=PIPE, stderr=PIPE)
         stdout, stderr = p.communicate()
         if p.returncode != 0:
@@ -90,9 +94,10 @@ def eval_results(judgement_file_path):
     #print_debug('Eval Success!!')
 
 
-def run_all(function_file_path, index_path, query_file_path, judgement_file_path):
+def run_all(function_file_path, index_path, query_file_path, judgement_file_path, 
+        pertube_type='0', pertube_paras_str=''):
     replace_and_make(function_file_path)
-    run_query(index_path, query_file_path)
+    run_query(index_path, query_file_path, pertube_type, pertube_paras_str)
     eval_results(judgement_file_path)
 
 
@@ -100,7 +105,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-a", "--all",
-        nargs=4,
+        nargs=6,
         help="run all of the functionalities: make, run query, eval")
 
     parser.add_argument("-m", "--replace_and_make",
@@ -108,7 +113,7 @@ if __name__ == '__main__':
         help="Replace and Make the new IndriRunQuery binary")
 
     parser.add_argument("-r", "--run_query",
-        nargs=2,
+        nargs=4,
         help="run query")
 
     args = parser.parse_args()
@@ -118,7 +123,10 @@ if __name__ == '__main__':
         index_path = args.all[1]
         query_file_path = args.all[2]
         judgement_file_path = args.all[3]
-        run_all(function_file_path, index_path, query_file_path, judgement_file_path)
+        pertube_type = args.all[4]
+        pertube_paras_str = args.all[5]
+        run_all(function_file_path, index_path, query_file_path, 
+            judgement_file_path, pertube_type, pertube_paras_str)
 
     if args.replace_and_make:
         file_path = args.replace_and_make[0]
@@ -127,4 +135,6 @@ if __name__ == '__main__':
     if args.run_query:
         index_path = args.run_query[0]
         query_file_path = args.run_query[1]
-        run_query(index_path, query_file_path)
+        pertube_type = args.run_query[2]
+        pertube_paras_str = args.run_query[3]
+        run_query(index_path, query_file_path, pertube_type, pertube_paras_str)
